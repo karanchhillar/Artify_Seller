9c11115dfc49a579907ae57ec133373c497ec9d5
karanchhillar <karan.5716403221@ipu.ac.in>
2023-08-11 13:34:19 +0530

    profile issue again(link issue) item upload added

--- app/src/main/java/com/example/artifyseller/ItemAdd.kt
+++ app/src/main/java/com/example/artifyseller/ItemAdd.kt
@@ -9,20 +9,18 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.ViewModelProvider
-import com.example.artifyseller.data.User
 import com.example.artifyseller.data.UserItemAdd
 import com.example.artifyseller.databinding.ActivityItemAddBinding
 import com.example.artifyseller.viewmodel.ViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import java.io.ByteArrayOutputStream
 import java.util.Date
 
@@ -34,6 +32,7 @@ class ItemAdd : AppCompatActivity() {
     private lateinit var storage: FirebaseStorage
     private var selectedImg: Uri? = null
     private lateinit var vm : ViewModel
+    private var photoClicked : Int = 0
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,9 +48,9 @@ class ItemAdd : AppCompatActivity() {
 
         binding.saveItemButton.setOnClickListener {
             // this function also include the code to store data in fire store
-            uploadImageToStorage(imageBitmap)
+            uploadImageToStorage(imageBitmap = null)
 
-            val intent = Intent(this , ItemFragment::class.java)
+            val intent = Intent(this , MainActivity::class.java)
             startActivity(intent)
         }
 
@@ -60,36 +59,50 @@ class ItemAdd : AppCompatActivity() {
         }
     }
     // code to send image to storage and the getting uri
-    fun uploadImageToStorage(imageBitmap : Bitmap){
-        auth = FirebaseAuth.getInstance()
-        vm = ViewModelProvider(this).get(ViewModel::class.java)
-        val baos = ByteArrayOutputStream()
-        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-        val data = baos.toByteArray()
+    fun uploadImageToStorage(imageBitmap: Bitmap?){
+        if (photoClicked == 1){
+            auth = FirebaseAuth.getInstance()
+            vm = ViewModelProvider(this).get(ViewModel::class.java)
+            val baos = ByteArrayOutputStream()
+            imageBitmap?.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            val data = baos.toByteArray()
 
-        storage = FirebaseStorage.getInstance()
-        val storageRef = storage.reference
+            storage = FirebaseStorage.getInstance()
+            val storageRef = storage.reference
 
-        val storagePath = storageRef.child("Photos/${Date().time.toString()}.jpg")
-        val uploadTask = storagePath.putBytes(data)
+            val storagePath = storageRef.child("Photos/${Date().time.toString()}.jpg")
+            val uploadTask = storagePath.putBytes(data)
 
-        uploadTask.addOnSuccessListener { it ->
-            val task = it.metadata?.reference?.downloadUrl
-            task?.addOnSuccessListener {
-                selectedImg = it
-                val user_item_data = UserItemAdd(binding.productNameTextView.text.toString() ,
-                    binding.priceTextView.text.toString().toInt() ,
-                    binding.availableQuantityTextView.text.toString() ,
-                    binding.productDescription.text.toString(),
-                    selectedImg.toString())
+            uploadTask.addOnSuccessListener { it ->
+                val task = it.metadata?.reference?.downloadUrl
+                task?.addOnSuccessListener {
+                    selectedImg = it
+                    val user_item_data = UserItemAdd(binding.productNameTextView.text.toString() ,
+                        binding.priceTextView.text.toString().toInt() ,
+                        binding.availableQuantityTextView.text.toString() ,
+                        binding.productDescription.text.toString(),
+                        selectedImg.toString())
 
-                vm.upload_item_data(user_item_data)
-                Toast.makeText(this, "$it", Toast.LENGTH_SHORT).show()
-            }
-                ?.addOnFailureListener {
-                    Toast.makeText(this, "task failed", Toast.LENGTH_SHORT).show()
+                    uploadItem(user_item_data)
+                    Toast.makeText(this, "New product added", Toast.LENGTH_SHORT).show()
                 }
+                    ?.addOnFailureListener {
+                        Toast.makeText(this, "task failed", Toast.LENGTH_SHORT).show()
+                    }
+            }
+        }else if (photoClicked == 0){
+            vm = ViewModelProvider(this).get(ViewModel::class.java)
+            val user_item_data = UserItemAdd(binding.productNameTextView.text.toString() ,
+                binding.priceTextView.text.toString().toInt() ,
+                binding.availableQuantityTextView.text.toString() ,
+                binding.productDescription.text.toString(),
+                selectedImg.toString())
+
+            uploadItem(user_item_data)
+            Toast.makeText(this, "New product added", Toast.LENGTH_SHORT).show()
         }
+
+
     }
 
     // code for choosing btw camera and media
@@ -151,4 +164,48 @@ class ItemAdd : AppCompatActivity() {
             }
         }
     }
+
+private fun uploadItem(item : UserItemAdd) {
+//    var timeOfUpload : Long = Utils.getTime()
+//    var timeOfCompletion : Long = timeOfUpload + 86400
+    val firestore = FirebaseFirestore.getInstance()
+    val auth = FirebaseAuth.getInstance()
+    val itemAddRef = firestore.collection("item_add").document(auth.currentUser?.uid.toString())
+    itemAddRef.get().addOnSuccessListener {
+        if (it.exists()){
+            val productName = it.get("product_name") as List<*>
+            val newProduct = productName.toMutableList()
+            newProduct.add(item.productName)
+
+            val price = it.get("price") as List<*>
+            val newPrice = price.toMutableList()
+            newPrice.add(item.price)
+
+            val availableQuantity = it.get("available_quantity") as List<*>
+            val newQuantity = availableQuantity.toMutableList()
+            newQuantity.add(item.availableQuantity)
+
+            val productDescription = it.get("product_description") as List<*>
+            val newProductDescription = productDescription.toMutableList()
+            newProductDescription.add(item.productDescription)
+
+            val productImage = it.get("product_image") as List<*>
+            val newProductImage = productImage.toMutableList()
+            newProductImage.add(item.productImage)
+
+            itemAddRef.update(hashMapOf("product_name" to newProduct,"price" to newPrice,
+                "available_quantity" to newQuantity , "product_description" to newProductDescription
+                ,"product_image" to newProductImage) as Map<String, Any>)
+
+        }else{
+            val itemData = hashMapOf("product_name" to listOf(item.productName)
+                ,"price" to listOf(item.price)
+                ,"available_quantity" to listOf(item.availableQuantity)
+                , "product_description" to listOf(item.productDescription)
+                ,"product_image" to listOf(item.productImage))
+            itemAddRef.set(itemData)
+        }
+    }
+
+}
 }
